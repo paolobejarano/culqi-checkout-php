cd16a9ca377dbfe7d496e2c3d67927973d8e55e5
Paolo Bejarano <paolo@kurios.la>
2024-01-30 18:22:56 -0500

    Fix http request

--- checkout.php
+++ checkout.php
@@ -2,25 +2,6 @@
 
 $curl = curl_init();
 
-$data = array(
-    'amount' => '1500',  // Ingresar monto en céntimos
-    'currency_code' => 'PEN', // Ingresar moneda PEN o USD
-    'email' => 'paolo@aclaralab.com',  // Correo del cliente
-    'source_id' => 'ype_test_CjxTCrwPgeDoKd6K', // Ingresar token para realizar cobro aquí
-    'description' => 'PAGO ACLARA LAB' // Descripción en estado de cuenta (opcional)
-);
-
-
-$curl = curl_init();
-
-$data = array(
-    'amount' => '1500',  // Ingresar monto en céntimos
-    'currency_code' => 'PEN', // Ingresar moneda PEN o USD
-    'email' => 'paolo@aclaralab.com',  // Correo del cliente
-    'source_id' => 'ype_test_CjxTCrwPgeDoKd6K', // Ingresar token para realizar cobro aquí
-    'description' => 'PAGO ACLARA LAB' // Descripción en estado de cuenta (opcional)
-);
-
 curl_setopt_array($curl, array(
     CURLOPT_URL => 'https://api.culqi.com/v2/charges',
     CURLOPT_RETURNTRANSFER => true,
@@ -30,7 +11,14 @@ curl_setopt_array($curl, array(
     CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_POSTFIELDS => $data,
+    CURLOPT_POSTFIELDS =>'{
+    "amount": "1500",
+    "currency_code": "PEN",
+    "email": "paolo@gmail.com",
+    "source_id": "ype_test_BmsKSvs2n9eR0441",
+    "description": "PAGO ACLARA LAB"
+
+}',
     CURLOPT_HTTPHEADER => array(
         'Content-Type: application/json',
         'Authorization: Bearer sk_test_bb98e8f167e8c8c5'
@@ -38,6 +26,9 @@ curl_setopt_array($curl, array(
 ));
 
 $response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
 
 // Check for cURL errors
 if (curl_errno($curl)) {
@@ -54,42 +45,3 @@ if (curl_errno($curl)) {
         // Additional error handling for other status codes
     }
 }
-
-curl_close($curl);
-echo $response;
-curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://api.culqi.com/v2/charges',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_POSTFIELDS =>$data,
-    CURLOPT_HTTPHEADER => array(
-        'Content-Type: application/json',
-        'Authorization: Bearer sk_test_bb98e8f167e8c8c5'
-    ),
-));
-
-$response = curl_exec($curl);
-
-// Check for cURL errors
-if (curl_errno($curl)) {
-    echo 'Curl error: ' . curl_error($curl);
-} else {
-    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-    // El cargo se hizo solo si el código es 201
-    if ($httpCode == 201) {
-        echo 'Request successful (HTTP status 201 Created)';
-        // Additional handling for 201 status code, if needed
-    } else {
-        echo 'Request failed with HTTP status ' . $httpCode;
-        // Additional error handling for other status codes
-    }
-}
-
-curl_close($curl);
-echo $response;
